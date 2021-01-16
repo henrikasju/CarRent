70a4220634ed2c17893402894173eff832c95297
Henrikas <henrikasju@gmail.com>
2021-01-16 11:22:12 +0200

    Initial commit

---
+++ CarRent/Controllers/CarRentViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CarRent
+//
+//  Created by Henrikas J on 16/01/2021.
+//
+
+import UIKit
+
+class CarRentViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    navigationItem.title = "Rental Cars"
+    navigationController?.navigationBar.prefersLargeTitles = true
+
+
+    view.backgroundColor = .white
+  }
+
+
+}
+

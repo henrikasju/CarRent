273c715d52794d21481a3a4cfef899742b1b98b4
Henrikas <henrikasju@gmail.com>
2021-01-16 11:32:43 +0200

    Adding CarRentControllers main view

--- CarRent/Controllers/CarRentViewController.swift
+++ CarRent/Controllers/CarRentViewController.swift
@@ -9,16 +9,16 @@ import UIKit
 
 class CarRentViewController: UIViewController {
 
+  private let v = CarRentView()
+
+  override func loadView() {
+    view = v
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     navigationItem.title = "Rental Cars"
     navigationController?.navigationBar.prefersLargeTitles = true
-
-
-    view.backgroundColor = .white
   }
-
-
 }
-
---
+++ CarRent/Views/CarRent/CarRentView.swift
@@ -0,0 +1,17 @@
+//
+//  CarRentView.swift
+//  CarRent
+//
+//  Created by Henrikas J on 16/01/2021.
+//
+
+import UIKit
+
+class CarRentView: UIView {
+
+  convenience init() {
+    self.init(frame: .zero)
+
+    backgroundColor = .white
+  }
+}
